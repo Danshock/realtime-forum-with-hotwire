c0a1482f1d876e3719c83a7d262d6799c549b844
Danislav <danito1233@gmail.com>
2022-08-11 15:48:31 +0100

    Add Discussion model
    This commit adds the Discussion model and sets the association with
    user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,4 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true
+
+  has_many :discussions, dependent: :destroy
 end
