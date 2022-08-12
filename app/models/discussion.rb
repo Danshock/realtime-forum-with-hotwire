82374444a4551e5e892487d276d02f7031d7ac4c
Danislav <danito1233@gmail.com>
2022-08-12 14:10:45 +0100

    Add real time discussion functionality
    This commit adds a real time discussion functionality by utilising
    broadcast streams.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -5,6 +5,10 @@ class Discussion < ApplicationRecord
 
   validates_presence_of :name
 
+  after_create_commit -> { broadcast_prepend_to 'discussions' }
+  after_update_commit -> { broadcast_replace_to 'discussions' }
+  after_destroy_commit -> { broadcast_remove_to 'discussions' }
+
   def to_param
     "#{id}-#{name.downcase.to_s[0...50]}".parameterize
   end
