3272acada6eb55712848ff375554d450b3d9bcad
Danislav <danito1233@gmail.com>
2022-08-12 12:01:19 +0100

    Add discussion views and actions.
    This commit adds a few discussion views and actions to allow users to
    create and edit discussions via the UI.

---
+++ app/controllers/discussions_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class DiscussionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_discussion, only: %i[edit update]
+
+  def index
+    @discussions = Discussion.all
+  end
+
+  def new
+    @discussion = Discussion.new
+  end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+
+    respond_to do |format|
+      if @discussion.save
+        format.html { redirect_to discussions_path, notice: 'Discussion created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to discussions_path, notice: 'Discussion updated successfully' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def discussion_params
+    params.require(:discussion).permit(:name, :closed, :pinned)
+  end
+
+  def set_discussion
+    @discussion = Discussion.find(params[:id])
+  end
+end
