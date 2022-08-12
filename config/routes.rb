9031b8ea81f55c575dd0854382191527fe3e92fe
Danislav <danito1233@gmail.com>
2022-08-12 12:10:19 +0100

    Allow users to delete discussions
    This commit adds the functionality to allow users to delete discussions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'main#index'
-  resources :discussions, only: %i[index new create edit update]
+  resources :discussions, only: %i[index new create edit update destroy]
 end
