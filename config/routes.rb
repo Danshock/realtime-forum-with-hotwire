471e5ef2fb4bfc22582aadd3b0f4bea7de82ba45
Danislav <danito1233@gmail.com>
2022-08-18 09:26:56 +0100

    Add new post on discussion show view
    This commit allows the user to create a new post when viewing a
    discussion.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'main#index'
-  resources :discussions, only: %i[index show new create edit update destroy]
+  resources :discussions, only: %i[index show new create edit update destroy] do
+    resources :posts, only: [:create], module: :discussions
+  end
 end
