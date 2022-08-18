1c16755c3aed3445e8b7b69339519668853d4c6f
Danislav <danito1233@gmail.com>
2022-08-18 13:27:49 +0100

    Add categories to discussions index
    This commit adds a categories partial to the discussions index with
    realtime updates on category name and count.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -5,6 +5,8 @@ class Discussion < ApplicationRecord
   belongs_to :category, counter_cache: true, touch: true
   has_many :posts, dependent: :destroy
 
+  delegate :name, prefix: :category, to: :category, allow_nil: true
+
   validates_presence_of :name
 
   accepts_nested_attributes_for :posts
