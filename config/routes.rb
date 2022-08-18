3d390649ceb2283edee4653c8b71b560dd66c5e4
Danislav <danito1233@gmail.com>
2022-08-18 14:52:31 +0100

    Add category filtering
    This commit adds the functionality to filter by categories.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'main#index'
   resources :discussions, only: %i[index show new create edit update destroy] do
     resources :posts, only: %i[create show edit update destroy], module: :discussions
+
+    collection do
+      get 'category/:id', to: 'categories/discussions#index', as: :category
+    end
   end
   resources :categories
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  subject { FactoryBot.create(:category) }
+
   it { should have_many(:discussions).dependent(:nullify) }
+
+  context '#to_param' do
+    let(:expected_url) do
+      "#{subject.id}-#{subject.name.downcase.to_s[0...50]}".parameterize
+    end
+
+    it 'outputs a friendly url' do
+      expect(subject.to_param).to eq(expected_url)
+    end
+  end
 end
