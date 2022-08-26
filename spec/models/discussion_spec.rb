187487a1c7d344f052692b8ea489ba1b21966a8d
Danislav <danito1233@gmail.com>
2022-08-26 11:58:49 +0100

    Add toggle subscription status
    This commit updates the discussion model to have methods to toggle
    subscription status.

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -4,10 +4,20 @@ require 'rails_helper'
 
 RSpec.describe Discussion, type: :model do
   subject { FactoryBot.create(:discussion) }
+  let(:discussion_subscription) do
+    FactoryBot.create(
+      :discussion_subscription, :optin, discussion_id: subject.id
+    )
+  end
+  let(:user) { discussion_subscription.user }
 
   it { should belong_to(:user) }
   it { should belong_to(:category) }
   it { should have_many(:posts).dependent(:destroy) }
+  it { should have_many(:users).through(:posts) }
+  it { should have_many(:optin_subscribers).through(:discussion_subscriptions) }
+  it { should have_many(:optout_subscribers).through(:discussion_subscriptions) }
+  it { should have_many(:discussion_subscriptions).dependent(:destroy) }
   it { should validate_presence_of(:name) }
   it { should accept_nested_attributes_for(:posts) }
 
@@ -18,6 +28,24 @@ RSpec.describe Discussion, type: :model do
 
     it 'outputs a friendly url' do
       expect(subject.to_param).to eq(expected_url)
+    end
+  end
+
+  context '#subscribed_users' do
+    it 'returns an array with subscribers' do
+      expect(subject.subscribed_users).to be_a(Array)
+    end
+  end
+
+  context '#subscription_for' do
+    let(:user_nil) { nil }
+
+    it 'finds a discussion a user is subscribed for' do
+      expect(subject.subscription_for(user)).to be_instance_of(DiscussionSubscription)
+    end
+
+    it 'returns nil when user is nil' do
+      expect(subject.subscription_for(user_nil)).to be_nil
     end
   end
 end
