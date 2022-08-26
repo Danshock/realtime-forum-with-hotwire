850ec8ffab7e67c0a30d2d278f1614995d07e43a
Danislav <danito1233@gmail.com>
2022-08-26 13:27:04 +0100

    Display subscription status in UI
    This commit displays the subscription status in the UI.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -50,4 +50,36 @@ class Discussion < ApplicationRecord
       discussion_subscriptions.create(user: user, subscription_type: 'optin')
     end
   end
+
+  def subscribed?(user)
+    return false if user.nil?
+
+    if subscription = subscription_for(user)
+      subscription.subscription_type == 'optin'
+    else
+      posts.where(user_id: user.id).any?
+    end
+  end
+
+  def subscribed_reason(user)
+    return no_notifications_message if user.nil?
+
+    if subscription = subscription_for(user)
+      if subscription.subscription_type == 'optout'
+        "You're ignoring this thread."
+      elsif subscription.subscription_type == "optin"
+        "You're receiving notifications because you've subscribed to this thread."
+      end
+    elsif posts.where(user_id: user.id).any?
+      "You're receiving notifications because you've posted in this thread."
+    else
+      no_notifications_message
+    end
+  end
+
+  private
+
+  def no_notifications_message
+    "You're not receiving notifications from this thread"
+  end
 end
