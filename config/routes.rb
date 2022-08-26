a93d30f030552bf9c3651c0d7b4c54fdfe8be9f1
Danislav <danito1233@gmail.com>
2022-08-26 19:16:35 +0100

    Display notifications in UI
    This commit adds the functionality to display notifications in the UI
    and mark them as read.

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
     resources :notifications, only: ['create'], module: :discussions
   end
   resources :categories
+
+  resources :notifications, only: :index do
+    collection do
+      post '/mark_as_read', to: 'notifications#read_all', as: :read
+    end
+  end
 end
